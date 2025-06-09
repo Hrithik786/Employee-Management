cfec18c1b757ee0880c2dd263c62c4a47bec5aa9
Hrithik786 <sandy.3ahirwar@gmail.com>
2025-06-09 16:02:06 +0530

    dto improved

--- src/main/java/com/mediquity/employee/dto/EmployeeDTO.java
+++ src/main/java/com/mediquity/employee/dto/EmployeeDTO.java
@@ -21,6 +21,9 @@ import com.mediquity.employee.enums.Language;
 
 public class EmployeeDTO {
     
+    @NotNull
+    private int id;
+
     @NotBlank
     private String name;
     
@@ -52,86 +55,108 @@ public class EmployeeDTO {
     @Value("UNKNOWN")
     private BloodGroup bloodGroup;
 
-    public EmployeeDTO() {
-        super();
+    public EmployeeDTO(){
     }
-    public EmployeeDTO(String name, String email, String contactNum, Gender gender,
-                       MaritalStatus maritalStatus, int age, Nationality nationality,
-                       List<Language> language, BloodGroup bloodGroup) {
+    public EmployeeDTO(@NotNull int id, @NotBlank String name, @Email @NotBlank String email,
+            @NotBlank @Pattern(regexp = "\\d{10}", message = "Must be a 10-digit number") String contactNum,
+            @NotNull Gender gender, @NotNull @Min(18) @Max(65) int age, @NotNull MaritalStatus maritalStatus,
+            @NotEmpty List<Language> language, @NotNull Nationality nationality, BloodGroup bloodGroup) {
+        this.id = id;
         this.name = name;
         this.email = email;
         this.contactNum = contactNum;
         this.gender = gender;
-        this.maritalStatus = maritalStatus;
         this.age = age;
-        this.nationality = nationality;
+        this.maritalStatus = maritalStatus;
         this.language = language;
+        this.nationality = nationality;
         this.bloodGroup = bloodGroup;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public String getContactNum() {
         return contactNum;
     }
+
     public void setContactNum(String contactNum) {
         this.contactNum = contactNum;
     }
+
     public Gender getGender() {
         return gender;
     }
+
     public void setGender(Gender gender) {
         this.gender = gender;
     }
+
     public int getAge() {
         return age;
     }
+
     public void setAge(int age) {
         this.age = age;
     }
+
     public MaritalStatus getMaritalStatus() {
         return maritalStatus;
     }
+
     public void setMaritalStatus(MaritalStatus maritalStatus) {
         this.maritalStatus = maritalStatus;
     }
+
     public List<Language> getLanguage() {
         return language;
     }
+
     public void setLanguage(List<Language> language) {
         this.language = language;
     }
+
     public Nationality getNationality() {
         return nationality;
     }
+
     public void setNationality(Nationality nationality) {
         this.nationality = nationality;
     }
+
     public BloodGroup getBloodGroup() {
         return bloodGroup;
     }
+
     public void setBloodGroup(BloodGroup bloodGroup) {
         this.bloodGroup = bloodGroup;
     }
+
     @Override
     public String toString() {
-        return "EmployeeDTO [name=" + name + ", email=" + email + ", contactNum=" + contactNum + ", gender=" + gender
-                + ", age=" + age + ", maritalStatus=" + maritalStatus + ", language=" + language + ", nationality="
-                + nationality + ", bloodGroup=" + bloodGroup + ", getName()=" + getName() + ", getEmail()=" + getEmail()
-                + ", getContactNum()=" + getContactNum() + ", getGender()=" + getGender() + ", getAge()=" + getAge()
-                + ", getMaritalStatus()=" + getMaritalStatus() + ", getLanguage()=" + getLanguage()
-                + ", getNationality()=" + getNationality() + ", getBloodGroup()=" + getBloodGroup() + ", getClass()="
-                + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
-    }
-    
+        return "EmployeeDTO [id=" + id + ", name=" + name + ", email=" + email + ", contactNum=" + contactNum
+                + ", gender=" + gender + ", age=" + age + ", maritalStatus=" + maritalStatus + ", language=" + language
+                + ", nationality=" + nationality + ", bloodGroup=" + bloodGroup + "]";
+    }   
 }
